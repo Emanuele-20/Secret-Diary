e87dd49b67bb1728cbb17a97b2a8c7f351815269
Emanuele Pace <emanuele.10@outlook.it>
2021-08-04 16:18:08 +0100

    Created 2nd class to have higher class COHESION

--- diary.js
+++ diary.js
@@ -1,20 +1,64 @@
+class UnlockDiary {
+	unlock(diary) {
+		diary.padlockClose = false;
+	}
+
+	lock(diary) {
+		diary.padlockClose = true;
+	}
+}
 class SecretDiary {
 	constructor() {
 		this.padlockClose = true;
 		this.notes = [];
 	}
 
-	unlock() {
-		this.padlockClose = false;
-	}
-
-	getEntry(note) {
+	addEntry(note) {
 		if (this.padlockClose === true) {
 			throw new Error("The diary is locked !!!");
 		} else {
 			this.notes.push(note);
 		}
 	}
+
+	getEntries() {
+		if (this.padlockClose === true) {
+			throw new Error("The diary is locked !!!");
+		} else {
+			return this.notes.join();
+		}
+	}
 }
 
-module.exports = SecretDiary;
+module.exports = { SecretDiary, UnlockDiary };
+
+// CODE BEFORE CREATION OF 2 CLASSES
+
+// class SecretDiary {
+// 	constructor() {
+// 		this.padlockClose = true;
+// 		this.notes = [];
+// 	}
+
+// 	unlock() {
+// 		this.padlockClose = false;
+// 	}
+
+// 	addEntry(note) {
+// 		if (this.padlockClose === true) {
+// 			throw new Error("The diary is locked !!!");
+// 		} else {
+// 			this.notes.push(note);
+// 		}
+// 	}
+
+// 	getEntries() {
+// 		if (this.padlockClose === true) {
+// 			throw new Error("The diary is locked !!!");
+// 		} else {
+// 			return this.notes.join();
+// 		}
+// 	}
+// }
+
+// module.exports = SecretDiary;
--- diary.test.js
+++ diary.test.js
@@ -1,7 +1,8 @@
-const SecretDiary = require("./diary");
+const { SecretDiary, UnlockDiary } = require("./diary");
 
 beforeEach(() => {
 	diary = new SecretDiary();
+	lockpad = new UnlockDiary();
 });
 
 describe("Secret Diary", () => {
@@ -9,19 +10,40 @@ describe("Secret Diary", () => {
 		expect(diary.padlockClose).toBe(true);
 	});
 
-	it("Every diary has a entries container where save the notes", () => {
+	it("Every new instance has a note container", () => {
 		expect(diary.notes.length).toBe(0);
 	});
 
 	it("Can add a new note if the padlock is open", () => {
-		diary.unlock();
-		diary.getEntry();
+		lockpad.unlock(diary);
+		diary.addEntry("Buy milk");
 		expect(diary.notes.length).toBe(1);
 	});
 
-	it("Throw an error if the padlock is close", () => {
+	it("Throw an error if the padlock is close and we want add a note", () => {
 		expect(() => {
-			diary.getEntry();
+			diary.addEntry("Something");
+		}).toThrow("The diary is locked !!!");
+	});
+
+	it("Throw an error if the padlock is close and we want to get a note", () => {
+		expect(() => {
+			diary.getEntries();
+		}).toThrow("The diary is locked !!!");
+	});
+
+	it("Return an old note", () => {
+		lockpad.unlock(diary); // unlock diary
+		diary.addEntry("Buy milk"); // add a note
+		lockpad.lock(diary); // lock diary
+		lockpad.unlock(diary); // unlock diary
+		expect(diary.notes.length).toBe(1); // check the note container
+		expect(diary.getEntries()).toBe("Buy milk"); // check the note
+	});
+
+	it("Throw an error if the padlock is close and we want to get a note", () => {
+		expect(() => {
+			diary.getEntries();
 		}).toThrow("The diary is locked !!!");
 	});
 });
