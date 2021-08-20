142337e094056be0e670216d60b892666a2c84a5
Emanuele Pace <emanuele.10@outlook.it>
2021-08-20 10:59:01 +0100

    Test refactorin

--- test/diary2.test.js
+++ test/diary2.test.js
@@ -25,21 +25,22 @@ describe("Secret Diary", () => {
 	it("The diary instance born with a close padlock", () => {
 		expect(diary.padlock.close).toBe(true);
 	});
-	it("Unlock the diary and add a note", () => {
-		expect(diary.padlock.close).toBe(true);
+	it("Unlock the diary", () => {
 		diary.padlock.unlock();
 		expect(diary.padlock.close).toBe(false);
 	});
 	it("Can locks the padlock", () => {
 		diary.padlock.unlock();
 		expect(diary.padlock.close).toBe(false);
+		diary.padlock.lock();
+		expect(diary.padlock.close).toBe(true);
 	});
 	it("Can't lock a locked diary", () => {
 		expect(() => {
 			diary.padlock.lock();
 		}).toThrow("The diary is locked !!!");
 	});
-	it("Unlock the diary, add a note and check if the note array is update", () => {
+	it("Unlock the diary, add a note and check if the note array is updated", () => {
 		diary.padlock.unlock();
 		diary.addEntryes("some");
 		expect(diary.notes.length).toBe(1);
